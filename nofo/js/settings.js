a7eaf9fd0b3c83ce82975f1528ed66a3af332b59
DkFighT <116903571+DkFighT@users.noreply.github.com>
2023-03-03 22:33:44 +0300

    Add files via upload

--- nofo/js/settings.js
+++ nofo/js/settings.js
@@ -3,28 +3,94 @@ document.getElementById('filter-btn').style.display = 'none';
 
 function changetheme(th) {
     th.lastChild.onchange = function () {
-        if (th.lastChild.checked){
+        if (th.lastChild.checked) {
             th.style.background = 'url(../resources/sun.png)';
             th.style.backgroundPosition = 'center';
             th.style.backgroundRepeat = 'no-repeat';
             document.querySelector('.light').classList.add('dark');
             document.querySelector('.light').classList.remove('light');
             theme = false;
-            document.querySelectorAll('.menu-point').forEach(el =>{
+            document.querySelectorAll('.menu-point').forEach(el => {
                 el.firstChild.style.filter = "grayscale(100%) invert()";
             });
         }
-        else{
+        else {
             th.style.background = 'url(../resources/moon.png)';
             th.style.backgroundPosition = 'center';
             th.style.backgroundRepeat = 'no-repeat';
             document.querySelector('.dark').classList.add('light');
             document.querySelector('.dark').classList.remove('dark');
             theme = true;
-            document.querySelectorAll('.menu-point').forEach(el =>{
+            document.querySelectorAll('.menu-point').forEach(el => {
                 el.firstChild.style.filter = "grayscale(100%)";
             });
         }
 
     }
 }
+
+navigator.getBattery().then((battery) => {
+    function updateAllBatteryInfo() {
+        updateChargeInfo();
+        updateLevelInfo();
+        updateChargingInfo();
+        updateDischargingInfo();
+    }
+    updateAllBatteryInfo();
+
+    battery.addEventListener("chargingchange", () => {
+        updateChargeInfo();
+    });
+    function updateChargeInfo() {
+        let charge = document.getElementById('charge');
+        let persents = document.getElementById('persent');
+        if(battery.charging){
+            charge.style.display = 'flex';
+            persents.style.display = 'none';
+        } 
+        else{
+            charge.style.display = 'none';
+            persents.style.display = 'block';
+        }
+    }
+
+    battery.addEventListener("levelchange", () => {
+        updateLevelInfo();
+    });
+
+    function updateLevelInfo() {
+        let batary_level = document.getElementById('lvlbtry');
+        let persents = document.getElementById('persent');
+        if (battery.level * 100 < 20){
+            batary_level.style.backgroundColor = '#cc0000';
+        }
+        else{
+            batary_level.style.backgroundColor = '#00cc77';
+        }
+        batary_level.style.height = `${battery.level * 100}%`;
+        batary_level.style.top = `${100 - battery.level * 100}%`;
+        persents.textContent = `${battery.level * 100}%`;
+
+    }
+
+    battery.addEventListener("chargingtimechange", () => {
+        updateChargingInfo();
+    });
+    function updateChargingInfo() {
+        console.log(`Battery charging time: ${battery.chargingTime} seconds`);
+        
+    }
+
+    battery.addEventListener("dischargingtimechange", () => {
+        updateDischargingInfo();
+    });
+    function updateDischargingInfo() {
+        let chargtime = document.getElementById('chargtime');
+        if (battery.dischargingTime != 'Infinity'){
+        chargtime.textContent = `Left time: ${Math.floor(battery.dischargingTime / 60 / 60)}h : ${Math.floor(battery.dischargingTime % 60)}m`;
+        }
+        else{
+            chargtime.textContent = `Left time: ${battery.dischargingTime}`;
+        }
+    }
+});
